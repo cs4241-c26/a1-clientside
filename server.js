639e4c4cacb12efc87037448d6b1030070e08646
Wilson Wong <wwong2@wpi.edu>
2025-03-10 15:14:25 -0400

    initial assignment and template code

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' );
       break;
+    case '/hobby.html':
+      sendFile( response, 'hobby.html' );
+      break;
     default:
       response.end( '404 Error: File Not Found' );
   }
